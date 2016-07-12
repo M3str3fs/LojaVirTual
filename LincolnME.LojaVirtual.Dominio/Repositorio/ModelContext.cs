5d7e1e7d8617395fa17f2a5649bb1e2221c47c7c
M3str3fs <jsmterrestre@gmail.com>
2016-07-11 22:42:33 -0300

    Aula Virtual V6 Contexto e Repositorio

--- LincolnME.LojaVirtual.Dominio/Repositorio/ModelContext.cs
+++ LincolnME.LojaVirtual.Dominio/Repositorio/ModelContext.cs
@@ -16,7 +16,7 @@ namespace LincolnME.LojaVirtual.Dominio.Repositorio
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
-            modelBuilder.Entity<Produto>()
+            modelBuilder.Entity<Produto>().ToTable("Produtos");
         }
 
     }
---
+++ LincolnME.LojaVirtual/Controllers/ProdutoController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LincolnME.LojaVirtual.Dominio.Repositorio;
+
+namespace LincolnME.LojaVirtual.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        private ModelContext _repositorio;
+        // GET: Produto
+        public ActionResult Index()
+        {
+
+            _repositorio = new ModelContext();
+            var produtos = _repositorio.Produtos.Take(10);
+            return View(produtos);
+        }
+    }
+}
